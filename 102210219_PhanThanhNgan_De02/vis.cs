bad4359e01cdaedb42e3c8b492ab2ad6bef9eab0
Phan Thành Ngân <nngann2402@gmail.com>
2023-05-13 21:22:12 +0700

    up nagn

---
+++ 102210219_PhanThanhNgan_De02/vis.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _102210219_PhanThanhNgan_De02
+{
+    internal class vis
+    {
+    }
+}
